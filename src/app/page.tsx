8d6c9229c9cb807e4a01defa439be2cd971c9b1f
Sourabhkm7 <sourabhhithaldia@gmail.com>
2025-01-15 23:45:43 +0530

    Adding UI effects and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[.96] antialiased bd-grid-white/[0.02]">
-    <h1 className="text-2xl text-center">Music App using NextJs</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+    <HeroSection/>
     </main>
   );
 }
