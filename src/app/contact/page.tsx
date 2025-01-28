bc7f55fa677479c0c0081977d54546f71c266f3e
Sourabhkm7 <sourabhhithaldia@gmail.com>
2025-01-28 23:12:17 +0530

    Add Contact page with form for user inquiries and update Courses page link to LinkedIn

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+
 
 function page() {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-black py-12 pt-36">
+        <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">Contact Us</h1>
+        <p className="text-lg md:text-xl text-center font-sans mb-8 text-white" >We're here to help with questions aboutt our courses, programs, or events. <br />
+         Reach out and let us know how we can assist you in your musical journey.</p>
+         <form action="text"></form>
+    <div className="max-w-md mx-auto">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="message"
+            placeholder="Your message"
+            rows={4}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 items-center text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
     </div>
   )
 }
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -42,7 +42,7 @@ function page() {
                 <CardItem
                     translateZ={20}
                     as={Link}
-                    href="https://twitter.com/mannupaaji"
+                    href="https://www.linkedin.com/in/sourabhkm7/"
                     target="__blank"
                     className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
